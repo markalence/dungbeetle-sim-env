13e5b6136db88a4ffcee6b7793312d4eb69b4f16
Mark <mark.alence1@gmail.com>
2020-08-10 22:50:52 +0200

    set init bearing records actions

--- src/Beetle.js
+++ src/Beetle.js
@@ -1,17 +1,26 @@
 import * as THREE from 'three';
+import * as fs from 'file-saver'
+
+let demonstrationId = document.getElementById('filename');
+let initialBearing = document.getElementById('bearing');
+
 
 class Beetle {
     mesh;
-    keyHandler = {}
+    keyHandler = {'ArrowUp': false, 'ArrowLeft': false, 'ArrowRight': false}
+    keyStrokes = []
     static AOV = Math.PI / 4; // angle of view
     static LOV = 450; // length of view
 
     constructor() {
         let geometry = new THREE.CircleGeometry(25, 64);
         let material = new THREE.MeshBasicMaterial({color: 'red', transparent: true});
+        this.episodeOver = false;
+        this.episodeStarted = false;
         this.mesh = new THREE.Mesh(geometry, material);
         this.mesh.renderOrder = 999;
         this.mesh.add(this.setFov())
+        this.startTime = null;
     }
 
 
@@ -28,11 +37,32 @@ class Beetle {
         return new THREE.Mesh(geometry, material);
     }
 
+    writeFile() {
+        let misc = {initialBearing: initialBearing.value};
+        this.keyStrokes.unshift(JSON.stringify(misc) + '\n');
+        let blob = new Blob(this.keyStrokes, {type: 'application/json'});
+        fs.saveAs(blob, demonstrationId.value + '.txt');
+    }
+
     keyDown = () => {
 
-        if (this.keyHandler[37]) this.mesh.rotation.z += 0.05;
-        if (this.keyHandler[38]) this.mesh.translateY(3);
-        if (this.keyHandler[39]) this.mesh.rotation.z -= 0.05;
+        if (this.keyHandler['ArrowLeft']) this.mesh.rotation.z += 0.05;
+        if (this.keyHandler['ArrowUp']) this.mesh.translateY(3);
+        if (this.keyHandler['ArrowRight']) this.mesh.rotation.z -= 0.05;
+        if (this.keyHandler[' ']) this.episodeOver = true;
+
+        Object.keys(this.keyHandler).forEach(key => {
+            if (key !== 'ArrowLeft' && key !== 'ArrowUp' && key !== 'ArrowRight') delete this.keyHandler[key];
+            else {
+                if (!this.episodeStarted && this.keyHandler[key]) {
+                    this.episodeStarted = true;
+                    this.startTime = Date.now();
+                }
+            }
+        })
+        this.keyHandler['Time'] = Date.now() - this.startTime;
+        this.episodeStarted && this.keyStrokes.push(JSON.stringify(this.keyHandler) + '\n');
+        this.episodeOver && this.writeFile();
 
         if (this.mesh.rotation.z < 0 && this.mesh.rotation.z < -Math.PI) {
             this.mesh.rotation.set(0, 0, this.mesh.rotation.z + 2 * Math.PI)
--- src/index.js
+++ src/index.js
@@ -3,30 +3,52 @@ import Beetle from './Beetle'
 import Ball from "./Ball";
 import {Vector3} from "three";
 
-let HEIGHT = window.innerHeight;
-let WIDTH = window.innerWidth;
-let scene = new THREE.Scene();
-let camera = new THREE.OrthographicCamera(-WIDTH / 2, WIDTH / 2, HEIGHT / 2, -HEIGHT / 2, -1, 100);
-let renderer = new THREE.WebGLRenderer({antialias: true});
+const HEIGHT = window.innerHeight;
+const WIDTH = window.innerWidth;
+const scene = new THREE.Scene();
+const camera = new THREE.OrthographicCamera(-WIDTH / 2, WIDTH / 2, HEIGHT / 2, -HEIGHT / 2, -1, 100);
+const renderer = new THREE.WebGLRenderer({antialias: true});
+let initialBearing = document.getElementById('bearing');
+let bearing = 0
 let beetle = new Beetle()
 let balls = []
 let ballMeshes = []
 let visibleBalls = new Array(8).fill(0);
 
+initialBearing.addEventListener("change", e => {
+            bearing = Math.PI * initialBearing.value / 180;
+            balls.forEach(ball => {
+                scene.remove(ball.mesh)
+                ball.mesh.material.dispose();
+                ball.mesh.geometry.dispose();
+                balls = []
+            })
+            ballsInit()
+    }
+);
+
+
 function ballsInit() {
     let r = Math.PI
     let id = 0;
     for (let i = r; i > -r; i -= Math.PI / 4) {
-        balls.push(new Ball('circle', new THREE.Vector2(Math.cos(i) * 300, Math.sin(i) * 300), id));
+        balls.push(new Ball('circle',
+            new THREE.Vector2(
+                Math.cos(i - Math.PI / 2 - bearing) * 300,
+                Math.sin(i - Math.PI / 2 - bearing) * 300), id));
         id++;
     }
+    balls.forEach(db => {
+        scene.add(db.mesh)
+        ballMeshes.push(db.mesh);
+    })
 }
 
 function getIntersections() {
 
     balls.forEach(ball => {
         let dist = beetle.mesh.getWorldPosition(new Vector3()).distanceTo(ball.mesh.getWorldPosition(new Vector3()))
-        let angle = 0;
+        let angle;
         let H = beetle.mesh.clone(false).position.sub(ball.mesh.getWorldPosition(new Vector3()));
         if (Math.abs(H.x) < 0.01) {
             angle = H.y > 0 ? Math.PI / 2 : -Math.PI / 2;
@@ -60,21 +82,19 @@ function sceneInit() {
     renderer.setSize(WIDTH, HEIGHT);
     document.body.appendChild(renderer.domElement);
     renderer.setClearColor('#3f2a14', 1);
-    document.addEventListener("keydown", e => beetle.keyHandler[e.which] = true, true);
-    document.addEventListener("keyup", e => beetle.keyHandler[e.which] = false, true);
+    document.addEventListener("keydown", e => beetle.keyHandler[e.key] = true);
+    document.addEventListener("keyup", e => beetle.keyHandler[e.key] = false);
     ballsInit();
-    balls.forEach(db => {
-        scene.add(db.mesh)
-        ballMeshes.push(db.mesh);
-    })
+
 }
 
 sceneInit();
 
 function animate() {
+    if (beetle.episodeOver) return;
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
-    beetle.keyDown()
+    beetle.keyDown();
     getIntersections();
 }
 
