cbc7910fc68b784e0b48a9b600fcb2da52a611e3
Mark <mark.alence1@gmail.com>
2020-08-14 02:54:05 +0200

    moved intersections method to beetle class

--- src/Beetle.js
+++ src/Beetle.js
@@ -4,11 +4,12 @@ import {Vector3} from "three";
 
 let demonstrationId = document.getElementById('filename');
 
-
 class Beetle {
     mesh;
-    keyHandler
-    ballShapes = []
+    keyHandler;
+    balls = [];
+    visibleBalls = [];
+    ballShapes = [];
     stateActionPairs;
     groundTruth;
     static AOV = Math.PI / 4; // angle of view
@@ -25,14 +26,12 @@ class Beetle {
     }
 
     constructor() {
-
         this.reset();
         let geometry = new THREE.CircleGeometry(25, 64);
         let material = new THREE.MeshBasicMaterial({color: 'red', transparent: true});
         this.mesh = new THREE.Mesh(geometry, material);
         this.mesh.add(this.setFov())
         this.mesh.renderOrder = 999;
-
     }
 
 
@@ -59,7 +58,12 @@ class Beetle {
 
     writeFile() {
         this.stateActionPairs.unshift(JSON.stringify({shapes: this.ballShapes}) + '\n');
-        this.groundTruth.unshift(JSON.stringify({shapes: this.ballShapes}) + '\n');
+        this.balls.forEach(ball => {
+            this.groundTruth.unshift(JSON.stringify({
+                position: ball.mesh.position,
+                shape: ball.shape
+            }) + '\n');
+        })
         let stateActionBlob = new Blob(this.stateActionPairs, {type: 'application/json'});
         let groundTruthBlob = new Blob(this.groundTruth, {type: 'application/json'});
         fs.saveAs(stateActionBlob, demonstrationId.value + '_states&actions' + '.txt');
@@ -70,7 +74,7 @@ class Beetle {
     /**
      * Moves beetle according to the keys that are currently down
      */
-    moveBeetle(visibleBalls) {
+    moveBeetle() {
         if (this.fileWritten) return;
         if(!this.episodeOver) {
             if (this.keyHandler['ArrowLeft']) this.mesh.rotation.z += 0.05;
@@ -100,7 +104,7 @@ class Beetle {
                 //save state and action
                 this.stateActionPairs.push(JSON.stringify({
                     action: this.keyHandler,
-                    state: visibleBalls,
+                    state: this.visibleBalls,
                 }) + '\n');
             }
             //make sure that the magnitude of the rotation never exceeds PI
@@ -114,9 +118,51 @@ class Beetle {
         }
     }
 
-    setBallShapes(ballShapes){
-        this.ballShapes = ballShapes;
+    /**
+     * get the balls that are visible to the beetle
+     */
+    getIntersections() {
+        this.balls.forEach(ball => {
+            let dist = this.mesh.getWorldPosition(new Vector3()).distanceTo(ball.mesh.getWorldPosition(new Vector3()))
+            let angle;
+
+            //get angle between beetle and ball by getting the angle of their difference vector
+            let H = ball.mesh.getWorldPosition(new Vector3()).sub(this.mesh.clone(false).position);
+
+            //if the ball and the beetle have similar x values,
+            //set the angle between them to be 90 (if ball above beetle) or -90 (if ball below beetle)
+            if (Math.abs(H.x) < 0.01) angle = H.y > 0 ? Math.PI / 2 : -Math.PI / 2;
+
+                //if ball and beetle have similar y values,
+            //set the angle between them to be 0 (if ball left of beetle) or 180 (if ball right of beetle)
+            else if (Math.abs(H.y) < 0.01) angle = H.x > 0 ? 0 : Math.PI;
+            else angle = Math.atan2(H.y, H.x);
+
+            //correct the angle by adjusting for initial offset of balls (90 deg)
+            //and by rotation of beetle
+            angle -= Math.PI / 2 + this.mesh.rotation.z
+
+            //ensure angle is between {0 and PI} or {-PI and 0}
+            if (angle < -Math.PI) angle += 2 * Math.PI;
+            else if (angle > 0 && angle > Math.PI) angle -= 2 * Math.PI;
+
+            //if the ball lies in the field of view triangle, it is visible to the beetle
+            if ((Math.abs(angle) < Beetle.AOV + 0.01) && dist <= 25 + Beetle.LOV * Math.cos(Beetle.AOV) / Math.cos(angle)) {
+                ball.mesh.material.color.setHex(0xA9A9A9);
+                (this.visibleBalls)[ball.id] = {visible: true, distance: dist};
+            } else {
+                ball.mesh.material.color.setHex(0x000);
+                this.visibleBalls[ball.id] = {visible: false, distance: dist};
+            }
+
+            //if beetle touches ball, the episode is over
+            if (dist < 50.05) {
+                this.episodeOver = true
+            }
+        })
     }
+
+
 }
 
 export default Beetle;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import * as THREE from 'three'
 import Beetle from './Beetle'
 import Ball from "./Ball";
-import {Vector3} from "three";
 import Board from "./Board";
 
 const HEIGHT = window.innerHeight;
@@ -9,7 +8,6 @@ const WIDTH = window.innerWidth;
 const scene = new THREE.Scene();
 const camera = new THREE.OrthographicCamera(-WIDTH / 2, WIDTH / 2, HEIGHT / 2, -HEIGHT / 2, -1, 100);
 const renderer = new THREE.WebGLRenderer({antialias: true});
-
 let initialBearing = document.getElementById('bearing');
 let randomBall = document.getElementById('ball');
 let bearing = 0;
@@ -56,53 +54,15 @@ function ballsInit() {
                 Math.sin(i - Math.PI / 2 - bearing) * 300), id));
         id++;
     }
-    beetle.setBallShapes(ballShapes);
+
+    beetle.balls = balls;
+    beetle.ballShapes = ballShapes;
     balls.forEach(db => {
         board.mesh.add(db.mesh)
         ballMeshes.push(db.mesh);
     })
 }
 
-function getIntersections() {
-    balls.forEach(ball => {
-        let dist = beetle.mesh.getWorldPosition(new Vector3()).distanceTo(ball.mesh.getWorldPosition(new Vector3()))
-        let angle;
-
-        //get angle between beetle and ball by getting the angle of their difference vector
-        let H = ball.mesh.getWorldPosition(new Vector3()).sub(beetle.mesh.clone(false).position);
-
-        //if the ball and the beetle have similar x values,
-        //set the angle between them to be 90 (if ball above beetle) or -90 (if ball below beetle)
-        if (Math.abs(H.x) < 0.01) angle = H.y > 0 ? Math.PI / 2 : -Math.PI / 2;
-
-            //if ball and beetle have similar y values,
-        //set the angle between them to be 0 (if ball left of beetle) or 180 (if ball right of beetle)
-        else if (Math.abs(H.y) < 0.01) angle = H.x > 0 ? 0 : Math.PI;
-        else angle = Math.atan2(H.y, H.x);
-
-        //correct the angle by adjusting for initial offset of balls (90 deg)
-        //and by rotation of beetle
-        angle -= Math.PI / 2 + beetle.mesh.rotation.z
-
-        //ensure angle is between {0 and PI} or {-PI and 0}
-        if (angle < -Math.PI) angle += 2 * Math.PI;
-        else if (angle > 0 && angle > Math.PI) angle -= 2 * Math.PI;
-
-        //if the ball lies in the field of view triangle, it is visible to the beetle
-        if ((Math.abs(angle) < Beetle.AOV + 0.01) && dist <= 25 + Beetle.LOV * Math.cos(Beetle.AOV) / Math.cos(angle)) {
-            ball.mesh.material.color.setHex(0xA9A9A9);
-            visibleBalls[ball.id] = {visible: true, distance: dist};
-        } else {
-            ball.mesh.material.color.setHex(0x000);
-            visibleBalls[ball.id] = {visible: false, distance: dist};
-        }
-
-        //if beetle touches ball, the episode is over
-        if (dist < 50.05) {
-            beetle.episodeOver = true
-        }
-    })
-}
 
 function sceneInit() {
     camera.zoom = 0.65;
@@ -132,8 +92,8 @@ sceneInit();
 function animate() {
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
+    beetle.getIntersections();
     beetle.moveBeetle(visibleBalls);
-    getIntersections();
 }
 
 animate();
